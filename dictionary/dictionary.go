cdde5ff74c19fd840cb52e41b797ca10f8fdc7b0
Billy Joe KOM KAMZE <billykamze3@gmail.com>
2023-11-18 20:02:32 +0100

    Mise en place de la concurrence avec l'utilisation de goroutines et des channels

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -7,55 +7,66 @@ import (
 	"os"
 )
 
+// Entry représente une entrée dans le dictionnaire avec un mot et sa définition.
 type Entry struct {
-	Definition string `json:"definition"`
+	Word       string `json:"mot"`       // Le mot associé à l'entrée, utilisé comme clé dans la map
+	Definition string `json:"definition"` // La définition du mot
 }
 
+// String retourne une représentation textuelle de l'entrée (la définition du mot).
 func (e Entry) String() string {
 	return e.Definition
 }
 
+// Dictionary représente un dictionnaire qui associe des mots à leurs définitions.
 type Dictionary struct {
-	filename string
-	entries  map[string]Entry
+	filename string             // Nom du fichier dans lequel le dictionnaire est stocké
+	entries  map[string]Entry  // Map pour stocker les entrées du dictionnaire avec des mots comme clés
 }
 
+// New crée une nouvelle instance de Dictionary en chargeant les données à partir d'un fichier.
 func New(filename string) *Dictionary {
 	d := &Dictionary{
 		filename: filename,
 		entries:  make(map[string]Entry),
 	}
 	if err := d.loadFromFile(); err != nil {
-		fmt.Println("Error loading from file:", err)
+		fmt.Println("Erreur lors du chargement depuis le fichier:", err)
 	}
 	return d
 }
 
+// Add ajoute un mot et sa définition au dictionnaire.
 func (d *Dictionary) Add(word string, definition string) {
-	d.entries[word] = Entry{Definition: definition}
-	d.syncToFile()
+    // Utilisez le mot comme clé dans la map pour assurer l'unicité des mots
+    d.entries[word] = Entry{Word: word, Definition: definition}
+    d.syncToFile() // Synchronise le dictionnaire avec le fichier après l'ajout
 }
 
+// Get récupère l'entrée associée à un mot dans le dictionnaire.
 func (d *Dictionary) Get(word string) (Entry, error) {
 	entry, found := d.entries[word]
 	if !found {
-		return Entry{}, fmt.Errorf("word not found")
+		return Entry{}, fmt.Errorf("mot introuvable")
 	}
 	return entry, nil
 }
 
+// Remove supprime un mot et sa définition du dictionnaire.
 func (d *Dictionary) Remove(word string) {
 	delete(d.entries, word)
-	d.syncToFile()
+	d.syncToFile() // Synchronise le dictionnaire avec le fichier après la suppression
 }
 
+// List affiche tous les mots et leurs définitions dans le dictionnaire.
 func (d *Dictionary) List() {
-	fmt.Println("Words in the dictionary:")
+	fmt.Println("Mots dans le dictionnaire:")
 	for word, entry := range d.entries {
-		fmt.Printf("Word: %s\nDefinition: %s\n\n", word, entry.String())
+		fmt.Printf("Mot: %s\nDéfinition: %s\n\n", word, entry.String())
 	}
 }
 
+// saveToFile sauvegarde les données du dictionnaire dans un fichier au format JSON.
 func (d *Dictionary) saveToFile() error {
 	file, err := os.Create(d.filename)
 	if err != nil {
@@ -63,9 +74,9 @@ func (d *Dictionary) saveToFile() error {
 	}
 	defer file.Close()
 
-	// Utiliser l'option Indent pour formater le JSON avec une indentation
+	// Utilise l'option Indent pour formater le JSON avec une indentation
 	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "    ") // Utiliser quatre espaces pour l'indentation
+	encoder.SetIndent("", "    ") // Utilise quatre espaces pour l'indentation
 
 	if err := encoder.Encode(d.entries); err != nil {
 		return err
@@ -74,6 +85,7 @@ func (d *Dictionary) saveToFile() error {
 	return nil
 }
 
+// loadFromFile charge les données du dictionnaire à partir d'un fichier JSON.
 func (d *Dictionary) loadFromFile() error {
 	file, err := os.Open(d.filename)
 	if err != nil {
@@ -89,8 +101,9 @@ func (d *Dictionary) loadFromFile() error {
 	return nil
 }
 
+// syncToFile synchronise les données du dictionnaire avec le fichier.
 func (d *Dictionary) syncToFile() {
 	if err := d.saveToFile(); err != nil {
-		fmt.Println("Error syncing to file:", err)
+		fmt.Println("Erreur lors de la synchronisation avec le fichier:", err)
 	}
 }
--- main.go
+++ main.go
@@ -13,6 +13,26 @@ func main() {
 	d := dictionary.New("dictionary.json")
 	reader := bufio.NewReader(os.Stdin)
 
+	// Channels pour les opérations d'ajout et de suppression
+	addChan := make(chan dictionary.Entry)
+	removeChan := make(chan string)
+
+	// Goroutine pour traiter les opérations d'ajout
+	go func() {
+		for entry := range addChan {
+			d.Add(entry.Word, entry.Definition)
+			fmt.Println("Word added successfully!")
+		}
+	}()
+
+	// Goroutine pour traiter les opérations de suppression
+	go func() {
+		for word := range removeChan {
+			d.Remove(word)
+			fmt.Println("Word removed successfully!")
+		}
+	}()
+
 	for {
 		fmt.Println("1. Add Word")
 		fmt.Println("2. Define Word")
@@ -26,14 +46,16 @@ func main() {
 
 		switch choice {
 		case 1:
-			actionAdd(d, reader)
+			actionAdd(reader, addChan)
 		case 2:
 			actionDefine(d, reader)
 		case 3:
-			actionRemove(d, reader)
+			actionRemove(reader, removeChan)
 		case 4:
 			actionList(d)
 		case 5:
+			close(addChan)
+			close(removeChan)
 			os.Exit(0)
 		default:
 			fmt.Println("Invalid choice. Please choose a number between 1 and 5.")
@@ -41,7 +63,7 @@ func main() {
 	}
 }
 
-func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
+func actionAdd(reader *bufio.Reader, addChan chan<- dictionary.Entry) {
 	fmt.Print("Enter word: ")
 	word, err := reader.ReadString('\n')
 	if err != nil {
@@ -58,33 +80,42 @@ func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
 	}
 	definition = strings.TrimSpace(definition)
 
-	d.Add(word, definition)
-	fmt.Println("Word added successfully!")
+	// Envoyer les données au channel d'ajout
+	addChan <- dictionary.Entry{Word: word, Definition: definition}
 }
 
+func actionRemove(reader *bufio.Reader, removeChan chan<- string) {
+	fmt.Print("Enter word to remove: ")
+	word, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+	word = strings.TrimSpace(word)
+
+	// Envoyer le mot au channel de suppression
+	removeChan <- word
+}
 
 func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
 	fmt.Print("Enter word to define: ")
 	word, _ := reader.ReadString('\n')
 	word = word[:len(word)-1] // Remove the newline character
 
-	entry, err := d.Get(word)
-	if err != nil {
-		fmt.Println("Word not found.")
-	} else {
-		fmt.Println("Definition:", entry.String())
-	}
-}
-
-func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
-	fmt.Print("Enter word to remove: ")
-	word, _ := reader.ReadString('\n')
-	word = word[:len(word)-1] // Remove the newline character
-
-	d.Remove(word)
-	fmt.Println("Word removed successfully!")
+	// Utiliser une goroutine pour éviter le blocage du programme pendant la définition
+	go func() {
+		entry, err := d.Get(word)
+		if err != nil {
+			fmt.Println("Word not found.")
+		} else {
+			fmt.Println("Definition:", entry.String())
+		}
+	}()
 }
 
 func actionList(d *dictionary.Dictionary) {
-	d.List()
+	// Utiliser une goroutine pour éviter le blocage du programme pendant la liste
+	go func() {
+		d.List()
+	}()
 }
